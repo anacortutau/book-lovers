b5c7c51721f4e9d0cc1961f937b5549363c1de2e
Ana Corbalan Tutau <acortutau@gmail.com>
2024-04-28 12:39:20 +0200

    fix ChapterEntity

--- src/main/java/com/anuki/booklovers/models/ChapterEntity.java
+++ src/main/java/com/anuki/booklovers/models/ChapterEntity.java
@@ -1,9 +1,6 @@
 package com.anuki.booklovers.models;
 
-
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.*;
 import java.io.Serializable;
